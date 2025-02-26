557534c2362431c0b140b7af9fae661a8f663889
WilliamSoares21 <160546453+WilliamSoares21@users.noreply.github.com>
2025-02-26 17:44:34 +0000

    Testando o Post com filtro

--- first-spring-app/src/main/java/com/will/first_spring_app/controller/HelloWorldController.java
+++ first-spring-app/src/main/java/com/will/first_spring_app/controller/HelloWorldController.java
@@ -3,10 +3,16 @@ package com.will.first_spring_app.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;// Importa a anotação @Qualifier para dar nome ao bean
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.will.first_spring_app.domain.User;
 import com.will.first_spring_app.service.HelloWorldService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController // Indica que essa classe é um controller REST
 @RequestMapping ("/hello-world") // Indica que essa classe escuta todas as requisições que chegam na URL /hello-world
@@ -32,4 +38,11 @@ public class HelloWorldController {
     public String helloConfiguration(){
         return helloConfiguration + "Will";// Retorna a string injetada pelo bean
     }
+
+    @PostMapping("/{id}") // Indica que o método abaixo responde a requisições do tipo POST
+    public String helloWorldPost(@PathVariable("id") String id, @RequestParam(value = "filter", defaultValue = "nenhum" ) String filter, @RequestBody User body) { // O método abaixo recebe um id, um filtro e um objeto do tipo User
+        //return "Hello World POST " + body.getName() + id; // Retorna uma string concatenando o nome do usuário com o id
+        return "Hello World POST " + filter; // Retorna uma string concatenando o nome do usuário com o id;
+    }
+    
 }
